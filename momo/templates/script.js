986a96c0bc9fb8816ae7d744330e7a7c92609366
haward0519 <110471342+haward0519@users.noreply.github.com>
2023-11-20 21:39:39 +0900

    외관 다마무리

--- momo/templates/script.js
+++ momo/templates/script.js
@@ -7,43 +7,57 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             searchInput.value = request.query;
         }
     }
-});
+});window.onload = function() {
+    var targetContainer = document.querySelector('#rcnt');
+    if (!targetContainer) return;
 
-window.onload = function() {
-    console.log("된다");
-
-    // 페이지에 form 요소가 있는지 확인합니다.
-    var form = document.querySelector('form');
-    if (form) {
-        form.addEventListener('submit', function() {
-            let searchQuery = document.querySelector('input[name="q"]').value;
-            chrome.runtime.sendMessage({ action: "updateSearch", query: searchQuery });
-        });
-    }
-
-    console.log("진짜 된다");
-
-    var targetContainer;
-
-    var intervalID = setInterval(function() {
-        targetContainer = document.querySelector('#rcnt');
-        if (targetContainer) {
-
-            clearInterval(intervalID);
-            targetContainer.style.display = 'flex';
-            targetContainer.style.flexDirection = 'row';
-            console.log("진짜 진짜 된다");
-            var iframe = document.createElement('iframe');
-            iframe.src = chrome.runtime.getURL('home.html');
-            iframe.style.width = '431px';
-            iframe.style.height = '400px';
-            iframe.style.border = 'none';
-            iframe.style.display = 'flex';
-            console.log("진짜 진짜 2된다");
-            targetContainer.appendChild(iframe);
+    var checkInterval = setInterval(function() {
+        var rhsContainer = document.querySelector('#rhs');
+        if (rhsContainer) {
+            var iframe = createIframe();
+            insertIframe(iframe, rhsContainer, true);
+            clearInterval(checkInterval);
+        } else if (!iframeInDocument()) { // 이 함수로 iframe 존재 여부를 검사
+            var iframe = createIframe();
+            insertIframe(iframe, targetContainer, false);
         }
-    }, 1000); // 1초마다 #rcnt 요소를 찾습니다.
+    }, 1000);
+};
+
+function createIframe() {
+    var iframe = document.createElement('iframe');
+    iframe.src = chrome.runtime.getURL('home.html');
+    iframe.style.width = '431px';
+    iframe.style.height = '400px';
+    iframe.style.border = 'none';
+    return iframe;
 }
+
+function insertIframe(iframe, container, isRhsContainer) {
+    if (isRhsContainer) {
+        iframe.style.position = 'relative';
+        iframe.style.zIndex = 3;
+        // #rhs 컨테이너의 첫 번째 자식으로 iframe 삽입
+        if (container.firstChild) {
+            container.insertBefore(iframe, container.firstChild);
+        } else {
+            container.appendChild(iframe);
+        }
+    } else {
+        container.style.display = 'flex';
+        container.style.flexDirection = 'row';
+        iframe.style.display = 'flex';
+        iframe.style.marginleft = '50px';
+        container.appendChild(iframe);
+    }
+}
+function iframeInDocument() {
+    return document.querySelector('iframe[src="' + chrome.runtime.getURL('home.html') + '"]') != null;
+}
+
+
+
+
 
 
 document.getElementById("search-button-2").addEventListener("click", ()=>{
