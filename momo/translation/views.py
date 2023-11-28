2f3ddf67a5154f0b50117fe83e4054394ee68389
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2023-11-28 17:24:49 +0900

    reponse data user_input -> 'word'

--- momo/translation/views.py
+++ momo/translation/views.py
@@ -9,8 +9,7 @@ API_KEY = settings.API_KEY
 # Create your views here.
 def home(request):
     if request.method == 'GET':
-        # user_input = request.GET.get('word', False)
-        user_input = "재팬"
+        user_input = request.GET.get('word', False)
         print(user_input)
         import openai
         import os
@@ -28,7 +27,7 @@ def home(request):
                 # 중복인 게 있으면 data_list에 넣지 않음 (중복되는 게 3개면 그냥 7개 단어만 보여줌)
                 if output_list[i-1] !="중복":
                     data = {}
-                    data[user_input] = output_list[i-1]
+                    data['word'] = output_list[i-1]
                     output_info = Word.objects.get(word = output_list[i-1])
                     data["pronun"] = output_info.pronounciation
                     data["value"] = output_info.definition
@@ -101,7 +100,7 @@ def home(request):
                 else: 
                     find_duplicate.append(eng)
                     data = {}
-                    data[user_input] = eng
+                    data['word'] = eng
                     data["pronun"] = pronunciation
                     data["value"] = korean_meaning
                     data_list.append(data)
@@ -130,17 +129,17 @@ def home(request):
             #  data_list = 
             #             [
             # {
-            #     "애로건트": "Arrogant",
+            #     "word": "Arrogant",
             #     "pronun": "(애로건트)",
             #     "value": "거만한"
             # },
             # {
-            #     "애로건트": "Argonaut",
+            #     "word": "Argonaut",
             #     "pronun": "(아거노트)",
             #     "value": "모험가"
             # },
             # {
-            #     "애로건트": "Apartment",
+            #     "word": "Apartment",
             #     "pronun": "(아파트먼트)",
             #     "value": "아파트"
             # },
@@ -148,7 +147,7 @@ def home(request):
             #       .
             #       .
             # {
-            #     "애로건트": "Agreement",
+            #     "word": "Agreement",
             #     "pronun": "(어그리먼트)",
             #     "value": "합의, 동의"
             # }
